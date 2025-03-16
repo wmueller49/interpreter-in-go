8d5bb35c7748a4ef4c50e6e8d33d26cd67290051
wmueller49 <mueller.william49@gmail.com>
2025-03-16 17:54:58 -0400

    adding a little more to the lexer

--- token/token.go
+++ token/token.go
@@ -2,11 +2,6 @@ package token
 
 type TokenType string
 
-type Token struct {
-	Type    TokenType
-	Literal string
-}
-
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
@@ -32,3 +27,20 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType {
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
