862c751b968f99974e501d9f1a0174797a67c23c
wmueller49 <mueller.william49@gmail.com>
2025-09-24 07:29:31 -0400

    starting the evaluator and simple object representations

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"interpreter-in-go/lexer"
 	"interpreter-in-go/parser"
+	"interpreter-in-go/evaluator"
 )
 
 const PROMPT = ">> "
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer){
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
