58974cee6c508b7a74af086cccec93b7b816f0a0
wmueller49 <mueller.william49@gmail.com>
2025-09-30 06:49:53 -0400

    The grand finale

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A repository designed to track my progress in the book "Writing an Interpreter i
 - [x] Chapter 1: Lexing
 - [x] Chapter 2: Parsing
 - [x] Chapter 3: Evaluation
-- [ ] Chapter 4: Extending the Interpreter (*In Progress*)
+- [x] Chapter 4: Extending the Interpreter
--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "interpreter-in-go/object"
+import (
+	"fmt"
+	"interpreter-in-go/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -91,6 +94,14 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
